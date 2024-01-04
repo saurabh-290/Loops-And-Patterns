2812b90b41e43ecccdb5b7a0de8ea0f47c0fddb4
saurabh-290 <132060392+saurabh-290@users.noreply.github.com>
2024-01-04 23:46:09 +0530

    Add files via upload

---
+++ Loops And Patterns.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout <<"Enter Your age"<<endl;
+    cin >> age;
+    if (age >= 18)
+    {
+        cout <<"You are Elasibal For Vote "<<endl;
+    }
+
+    else 
+    {
+        cout <<"You can't Vote"<<endl;
+    }
+     
+    return 0;
+}
